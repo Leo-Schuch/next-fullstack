30f5f0e4402071d5bdb778bbe3caede1d461626b
DESKTOP-5SU2620\Usuario <leoschuch23@gmail.com>
2023-05-03 20:15:07 -0300

    initial projects

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Box from "../src/components/Box";
+import theme from "../src/theme/theme";
+
+export default function HomeScreen(){
+  return(
+    <Box
+      styleSheet={{
+        fontFamily: theme.typography.fontFamily
+        
+      }} 
+      tag='main'
+    >
+      teste
+    </Box>
+  )
+}
