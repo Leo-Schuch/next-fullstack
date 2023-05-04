8b261dcf8affbf8d03006e082e8fc823f2ccf1f4
DESKTOP-5SU2620\Usuario <leoschuch23@gmail.com>
2023-05-03 21:47:55 -0300

    added font bold with styleSheet

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import Box from "@src/components/Box";
 import theme from "@src/theme/theme";
-import styled from "styled-components";
-
-
 
 export default function HomeScreen() {
   return (
@@ -10,13 +7,15 @@ export default function HomeScreen() {
       styleSheet={{
         fontFamily: theme.typography.fontFamily,
         backgroundColor: {
-          xs: 'red',
-          sm: 'blue',
-          lg: 'yellow'
-        }
+          xs: "red",
+          sm: "blue",
+          lg: "yellow",
+        },
+        fontWeight: theme.typography.fontWeight,
       }}
       tag="main"
-    >  
+    >
+      <p>oi</p>
     </Box>
   );
 }
